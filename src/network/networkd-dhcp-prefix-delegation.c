a144e8c054b5287e8d477a701abfa0f1f19c14fd
Cassandra Comar <cassandra@ditto.live>
2023-03-20 11:58:45 -0400

    add logging for debug

--- src/network/networkd-dhcp-prefix-delegation.c
+++ src/network/networkd-dhcp-prefix-delegation.c
@@ -3,6 +3,7 @@
 #include <linux/ipv6_route.h>
 #include <stdint.h>
 
+#include "log-link.h"
 #include "sd-dhcp6-client.h"
 
 #include "hashmap.h"
@@ -570,9 +571,11 @@ static int dhcp_pd_assign_subnet_prefix(
                                               "Failed to assign/update route for prefix %s: %m", pretty);
 
         uint64_t i;
-        for (i=0; ++i < UINT64_C(1) << (64-subnet_prefix_len);) {
+        for (i=0; ++i < UINT64_C(1) << (64 - subnet_prefix_len);) {
                 struct in6_addr *address = newdup(struct in6_addr, &prefix, 1);
                 address->s6_addr32[0] |= i;
+                const char *address_pretty = IN6_ADDR_PREFIX_TO_STRING(&prefix, subnet_prefix_len);
+                log_link_debug(link, "Requesting address %s from DHCP-PD prefix %s", address_pretty, pretty);
                 r = dhcp_pd_request_address(link, address, 64, lifetime_preferred_usec, lifetime_valid_usec);
                 if (r < 0)
                         return log_link_warning_errno(
